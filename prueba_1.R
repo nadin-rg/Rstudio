466a056df3505e4067b124b0ab9f0ca44c1c1224
nadinson ramos <nadinsonramos@gmail.com>
2020-07-07 12:07:15 -0500

    este es mi primer proyecto

---
+++ prueba_1.R
@@ -0,0 +1,3 @@
+print("ese es mi primer script con R ")
+print("este archivo fue creado con la finalidad de probar la conexion entre RStudio y gitHub")
+print("espero este archivo pueda vivir en gitHub")
